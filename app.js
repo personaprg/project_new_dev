877cca9a78ec3b599220fef0c47134d12e737786
동현 <Garry@example.com>
2023-11-14 15:45:07 +0900

    fs 모듈 관련 에러 (fs모듈 주석처리)

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express;
 const port = 8080;
-const fs = require(fs); //보류
+// const fs = require(fs); //보류
+// ReferenceError: Cannot access 'fs' before initialization 애러 발생지점
 
 app.use(express.static("public"));
 
