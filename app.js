ea999fcb8c76bf20202a2e135ba306399b21adcb
동현 <Garry@example.com>
2023-11-14 15:46:31 +0900

    public 타입에러 발생

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express;
 const port = 8080;
 // const fs = require(fs); //보류
-// ReferenceError: Cannot access 'fs' before initialization 애러 발생지점
+// ReferenceError: Cannot access 'fs' before initialization 에러 발생지점
 
 app.use(express.static("public"));
+//TypeError: app.use is not a function 타입 에러 발생, GPT에 질문 요청
+//질문
+//
+
 
 app.use('/', (req, res) =>{
   res.sendfile("index.html");
